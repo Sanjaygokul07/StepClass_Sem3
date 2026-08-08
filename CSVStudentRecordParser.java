2ded2890f40dff716d323b52772256aa9a22b6a4
Sanjaygokul07 <gunasekaranmilk@gmail.com>
2026-08-08 15:45:55 +0530

    Add files via upload

--- CSVStudentRecordParser.java
+++ CSVStudentRecordParser.java
@@ -1,1 +1,19 @@
-# StepClass_Sem3
+public class CSVStudentRecordParser {
+
+    // Method to parse a CSV line into a formatted student record
+    static void parseStudentRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        System.out.println("Name: " + fields[0] + " | Roll No: " + fields[1] + " | Dept: " + fields[2]);
+    }
+
+    public static void main(String[] args) {
+        parseStudentRecord("SANJAY GOKUL,RA2511032010018,CSE");
+        parseStudentRecord("SANJAY GOKUL,CSE");
+    }
+}
--- VowelConsonantCounter.java
+++ VowelConsonantCounter.java
@@ -1,1 +1,23 @@
-# StepClass_Sem3
+public class VowelConsonantCounter {
+
+    // Method to count vowels and consonants
+    static void countVowelsAndConsonants(String text) {
+        int vowels = 0, consonants = 0;
+        String vowelSet = "aeiouAEIOU";
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch == ' ') continue;
+            if (vowelSet.indexOf(ch) != -1) {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels + " | Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+        countVowelsAndConsonants("Java Programming");
+    }
+}
